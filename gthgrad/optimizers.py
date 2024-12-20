0c1e77cf206e91cddcfe7579cbcae5c784c2a283
gth <hadriengayap@gmail.com>
2024-12-19 21:59:28 -0400

    optimize SGD

--- gthgrad/optimizers.py
+++ gthgrad/optimizers.py
@@ -6,23 +6,27 @@ class SGD:
     """
     Stochastic Gradient Descent optimizer.
     """
-    def __init__(self, parameters, lr=0.01):
+    def __init__(self, parameters, lr=0.01,momentum=0.0):
         """
         Initializes the SGD optimizer.
 
         Args:
             parameters (list of Value): Parameters to optimize.
             lr (float, optional): Learning rate.
+            momentum: maintain direction of the optimization
         """
         self.parameters = parameters
+        self.momentum = momentum
         self.lr = lr
+        self.velocities = [0.0] + len(parameters)
 
     def step(self):
         """
         Performs a single optimization step.
         """
-        for p in self.parameters:
-            p.data += -self.lr * p.grad
+        for i,p in enumerate(self.parameters):
+            self.velocities[i] = self.momentum * self.velocities[i] - self.lr * p.grad
+            p.data += self.velocities[i]
 
     def zero_grad(self):
         """
